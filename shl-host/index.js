6f123035f0e6459fd1fce94b3466d778cfc7dea9
Sean Nolan <sean@thenolans.com>
2023-06-10 10:12:59 -0700

    multi

--- shl-host/index.js
+++ shl-host/index.js
@@ -7,6 +7,9 @@ import fs from "fs";
 import express from "express";
 import sanitize from "sanitize-filename";
 
+const GITHUB_URL_PREFIX =
+	  "https://raw.githubusercontent.com/seanno/shc-demo-data/main/cards/";
+
 // +----------------+
 // | handleManifest |
 // +----------------+
@@ -30,6 +33,44 @@ const handleManifest = (req, res) => {
 	  {
 		"contentType": "application/smart-health-card",
 		"location": payloadJson.url
+	  }
+	]
+  };
+
+  res.json(manifestJson);
+}
+
+// +---------------------+
+// | handleMultiManifest |
+// +---------------------+
+
+const handleMultiManifest = (req, res) => {
+
+  console.log("handleMultiManifest");
+  
+  if (req.query.passcode && req.query.passcode !== req.body.passcode) {
+	res.status(401).end();
+	return;
+  }
+
+  const payloadPath = cardFile(req.query.card, 'shl.json');
+  if (!payloadPath) { res.status(500).end(); return; }
+  
+  const payloadJson = JSON.parse(fs.readFileSync(payloadPath));
+
+  const manifestJson = {
+	"files": [
+	  {
+		"contentType": "application/smart-health-card",
+		"location": GITHUB_URL_PREFIX + "carin-revocable/jws.txt"
+	  },
+	  {
+		"contentType": "application/smart-health-card",
+		"location": GITHUB_URL_PREFIX + "carin-revoked/jws.txt"
+	  },
+	  {
+		"contentType": "application/fhir+json",
+		"location": GITHUB_URL_PREFIX + "multi-shl/ips1.enc"
 	  }
 	]
   };
@@ -62,6 +103,7 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/manifest", handleManifest);
+app.post("/multiManifest", handleMultiManifest);
 
 const port = process.env.PORT || 3001;
 
